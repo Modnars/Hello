ace89487e21cac9aaa87b10ae05f40f33e058be1
ModnarShen <modnar@foxmail.com>
2022-05-22 23:46:43 +0800

    add practice code for CMakeTutorial:Step1

--- CMake/Tutorial/src/Step1/tutorial.cxx
+++ CMake/Tutorial/src/Step1/tutorial.cxx
@@ -1,22 +1,24 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <libgen.h>
 
-int main(int argc, char* argv[])
-{
-  if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+#include "TutorialConfig.h"
 
-  // convert input to double
-  const double inputValue = atof(argv[1]);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // report version
+        std::cout << basename(argv[0]) << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << basename(argv[0]) << " number" << std::endl;
+        return 1;
+    }
 
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+    // convert input to double
+    const double inputValue = std::stod(argv[1]);
+
+    // calculate square root
+    const double outputValue = std::sqrt(inputValue);
+    std::cout << "The square root of " << inputValue << " is " << outputValue << std::endl;
+    return 0;
 }
